655258142490aa9eba6fb3f00ca5ecbb45c439cb
surabhi <surbhi.mishra.sbg@gmail.com>
2022-01-17 16:40:50 +0530

    Adding the new project

---
+++ First APP/First App/First App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using First_App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace First_App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
